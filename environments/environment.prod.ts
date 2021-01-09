d2b419ab38a684f84e4dc55be4c8f8925115a347
Philippe Malric <malric.philippe@gmail.com>
2021-01-09 13:52:41 -0500

    projet-de-loi2

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -7,14 +7,14 @@ export const environment = {
   test: false,
   i18nPrefix: '',
   firebaseConfig: {
-    apiKey: 'AIzaSyA3GnJ_6h6bdhobeOYIfW5_G_LbyZCPbtg',
-    authDomain: 'text-editor-by-section.firebaseapp.com',
-    databaseURL: 'https://text-editor-by-section.firebaseio.com',
-    projectId: 'text-editor-by-section',
-    storageBucket: 'text-editor-by-section.appspot.com',
-    messagingSenderId: '376881216693',
-    appId: '1:376881216693:web:b8fa933b6d3444078e12fc',
-    measurementId: 'G-C5X2SPF1LN'
+    apiKey: "AIzaSyCVlnQHcOIxolrHSS1d3MCGvnxvhksnXAA",
+    authDomain: "projet-de-loi-accq.firebaseapp.com",
+    databaseURL: "https://projet-de-loi-accq.firebaseio.com",
+    projectId: "projet-de-loi-accq",
+    storageBucket: "projet-de-loi-accq.appspot.com",
+    messagingSenderId: "1072563244873",
+    appId: "1:1072563244873:web:2b83f2f5e7d004f9605498",
+    measurementId: "G-L5ZFGVJBP8"
   },
   versions: {
     app: packageJson.version,
