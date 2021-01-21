951b536e68548e8d9dffcfd32a49075c5dff040b
Philippe Malric <malric.philippe@gmail.com>
2021-01-21 00:01:53 -0500

    admin-graph

--- projects/text_editor_by_section/src/app/components/admin/admin.component.ts
+++ projects/text_editor_by_section/src/app/components/admin/admin.component.ts
@@ -16,7 +16,7 @@ export class AdminComponent implements OnInit {
   dataSource:MatTableDataSource<String>;
   dataSource_propositions:MatTableDataSource<String>;
   users: String[];
-  displayedColumns: string[] = ['name1','txt1',"plus1","egale1","moins1","button1"];
+  displayedColumns: string[] = ['name1','txt1',"graph","plus1","egale1","moins1","button1"];
   displayedColumns2: string[] = ['name2','txt2','delete2'];
   displayedColumns3: string[] = ['name3',"plus3","egale3","moins3","delete3"];
   sub1: Subscription;
