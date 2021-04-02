6562802e85effccdbe63ccccfa928096c27cbea3
Philippe <malric.philippe@gmail.com>
2021-04-02 11:53:28 -0400

    phase3_1

--- projects/text_editor_by_section/src/app/app-routing.module.ts
+++ projects/text_editor_by_section/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ import { GoogleSheetComponent } from './components/google-sheet/google-sheet.com
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'message',
+    redirectTo: 'mode_d_emploi',
     pathMatch: 'full'
   },
   {
@@ -83,7 +83,7 @@ const routes: Routes = [
   
   {
     path: '**',
-    redirectTo: 'message'
+    redirectTo: 'mode_d_emploi'
   }
 ];
 
--- projects/text_editor_by_section/src/app/components/proposition-nav/proposition-nav.component.ts
+++ projects/text_editor_by_section/src/app/components/proposition-nav/proposition-nav.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { map, take, withLatestFrom } from 'rxjs/operators';
 import { GameService } from '../../services/game.service';
 import * as saveAs from 'file-saver';
+import { GoogleSheetService } from '../../services/google-sheet.service';
 
 //declare var saveAs:any;
 
@@ -17,9 +18,17 @@ export class PropositionNavComponent implements OnInit {
   @Input('data') data: any;
   data2: Observable<any>;
   myItem: any[];
+  subAllPrps: any;
+  props: unknown;
+  mapNom_to_non_delete_prop: {};
+  votes: unknown;
+  minus: {};
+  egale: {};
+  plus: {};
+  sub1: any;
   
 
-  constructor(private gameService:GameService) {
+  constructor(private googleSheetService : GoogleSheetService,private gameService:GameService) {
 
    
 
@@ -101,20 +110,78 @@ export class PropositionNavComponent implements OnInit {
   }
 
   downloadPDL() {
-    this.gameService.get_projet_de_loi3().pipe(take(1)).subscribe(((data:any)=>{
-      console.log(data)
+
+    this.subAllPrps = this.gameService.get_props2().pipe(take(1)).pipe(
+      withLatestFrom(this.gameService.get_upvote())).subscribe(([props,votes])=>{
+
+      console.log("props")
+      console.log(props)
+      this.props = props
+
+      this.mapNom_to_non_delete_prop = {}
+
+      for(let e of Object.keys(props)){
+        this.mapNom_to_non_delete_prop[e] = Object.keys(props[e]).filter((key)=>{
+
+          return ! props[e][key].delete
+
+        }).length
+
+
+
+      }
+      console.log("votes")
+      console.log(votes)
+      this.votes = votes
+
+      this.minus = {}
+      this.egale = {}
+      this.plus = {}
+      for(let e of Object.keys(votes)){
+        this.minus[e] = Object.keys(votes[e]).filter((key)=>{
+          return votes[e][key] == -1
+        })
+        
+        //console.log(e)
+        //console.log(this.minus[e])
+
+        this.egale[e] = Object.keys(votes[e]).filter((key)=>{
+          return votes[e][key] == 0
+        })
+        this.plus[e] = Object.keys(votes[e]).filter((key)=>{
+          return votes[e][key] == 1
+        })
+      }
+
+      this.googleSheetService.getCooker().pipe(take(1)).subscribe((items:any[])=>{
+        console.log("items44")
+        console.log(items)
+        
+        for(let e of items){
+          //e.props_non_delete = this.mapNom_to_non_delete_prop[e.nomunique]
+          //e.props_acc = 0
+          e.moins = (this.minus[e.nomunique])?this.minus[e.nomunique].length:0
+          e.egale = (this.egale[e.nomunique])?this.egale[e.nomunique].length:0
+          e.plus = (this.plus[e.nomunique])?this.plus[e.nomunique].length:0
+        }
+        
+        
+        console.log(items)
       const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
-      const header = Object.keys(data[0]);
-      let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(';'));
+      const header = Object.keys(items[0]);
+      let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(';'));
       csv.unshift(header.join(';'));
       let csvArray = csv.join('\r\n');
   
       var blob = new Blob([csvArray], {type: 'text/csv' })
-      saveAs.saveAs(blob, "myFile.csv");
-    }))
+      saveAs.saveAs(blob, "item-vote.csv");
+      })
+  
+    })
+
     
       
-   
+   //
   }
 
 
